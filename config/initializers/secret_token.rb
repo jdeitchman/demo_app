7503f28ab5911353c36490d3d3a267fbd9b67800
Joan Deitchman <jdeitchman@netflix.com>
2011-11-10 13:31:08 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'a05205e270211de05e0fe8714b3f0a287416165c71c5526637fb4c7a28ee2433d3cfaf10a11996bdba13240e0d79a7b4f16db15fc94f2aa5ce322842009315f6'
